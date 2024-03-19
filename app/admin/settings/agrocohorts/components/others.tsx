00c11ab2e493ed62c957ed72f1fe8d97f20bb2e6
MuhidinM <misbahmuhidin3@gmail.com>
2024-03-19 09:50:42 +0300

    all tabs are set up

---
+++ app/admin/settings/agrocohorts/components/others.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { CaretSortIcon } from "@radix-ui/react-icons";
+import { Edit, Plus } from "lucide-react";
+
+import { CapTableResponse } from "@/types/types";
+import CapTableFrom from "./forms/land-size-form";
+
+export const Others = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [addNew, setAddNew] = useState("");
+  const [capTables, setCapTables] = useState<CapTableResponse[]>([]);
+  const [capTable, setCapTable] = useState<CapTableResponse>();
+  const [updated, setUpdated] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  return (
+    <div className="grid w-full gap-4">
+      <div className="">
+        <h1 className="text-xl font-medium leading-tight text-cyan-500">
+          Others
+        </h1>
+        <CapTableFrom
+          setAddNew={setAddNew}
+          updated={updated}
+          setUpdated={setUpdated}
+          setLoading={setLoading}
+          loading={loading}
+          capTable={capTable}
+        />
+      </div>
+      <Collapsible
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        className="w-full space-y-2"
+      >
+        <div className="flex items-center justify-between px-1 space-x-4">
+          <h4 className="text-sm font-semibold">Land Size</h4>
+          <CollapsibleTrigger asChild>
+            <Button variant="ghost" size="sm">
+              <CaretSortIcon className="w-4 h-4" />
+              <span className="sr-only">Toggle</span>
+            </Button>
+          </CollapsibleTrigger>
+        </div>
+        <div className="flex space-x-2">
+          <div className="grid w-full grid-cols-2 gap-2">
+            <div className="px-4 py-2 text-sm font-semibold bg-gray-100 border rounded-md shadow-sm">
+              Interval
+            </div>
+            <div className="px-4 py-2 text-sm font-semibold bg-gray-100 border rounded-md shadow-sm">
+              Value
+            </div>
+          </div>
+        </div>
+        <CollapsibleContent className="space-y-2">
+          {capTables.map((item) => (
+            <div className="flex space-x-2" key={item.id}>
+              <div className="grid w-full grid-cols-3 gap-2">
+                <div className="px-4 py-2 font-mono text-sm border rounded-md shadow-sm">
+                  {item.month} Months
+                </div>
+                <div className="px-4 py-2 font-mono text-sm border rounded-md shadow-sm">
+                  {item.fixedRevenueShareRate}x of Principal
+                </div>
+                <div className="px-4 py-2 font-mono text-sm border rounded-md shadow-sm">
+                  {item.variableRevenueShareRate}x of Principal
+                </div>
+              </div>
+              <Button
+                size="icon"
+                variant="outline"
+                disabled={loading}
+                onClick={() => {
+                  setCapTable(item);
+                  setAddNew("returnCapTable");
+                }}
+              >
+                <Edit className="w-4 h-4" />
+              </Button>
+            </div>
+          ))}
+        </CollapsibleContent>
+        {addNew === "returnCapTable" && (
+          <CapTableFrom
+            setAddNew={setAddNew}
+            updated={updated}
+            setUpdated={setUpdated}
+            setLoading={setLoading}
+            loading={loading}
+            capTable={capTable}
+          />
+        )}
+      </Collapsible>
+    </div>
+  );
+};
--- app/admin/settings/agrocohorts/page.tsx
+++ app/admin/settings/agrocohorts/page.tsx
@@ -1,7 +1,94 @@
+"use client";
+
 import React from "react";
+import {
+  Cat,
+  DollarSign,
+  GraduationCap,
+  LandPlot,
+  LayoutDashboard,
+  Plus,
+  SlidersHorizontal,
+  Tractor,
+} from "lucide-react";
+
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Heading } from "@/components/ui/heading";
+import { LandSize } from "./components/land-size";
+import Summary from "./components/summary";
+import { FarmingExperience } from "./components/farming-experience";
+import { OtherIncome } from "./components/other-income";
+import { LivestockSize } from "./components/livestock-size";
+import { Education } from "./components/education";
+import { Others } from "./components/others";
+import { Status } from "./components/status";
 
 const AgrocChortsSettingsPage = () => {
-  return <div>AgrocChortsSettingsPage</div>;
+  return (
+    <div>
+      <div className="mb-4">
+        <Heading
+          title={`Settings`}
+          description="Manage all metrics from here"
+        />
+        <Separator className="my-2" />
+      </div>
+      <Tabs defaultValue="summary" className="w-full">
+        <TabsList className="flex items-center justify-start mb-4">
+          <TabsTrigger value="summary" className="py-1">
+            <LayoutDashboard className="w-5 h-5 mr-2" /> Summary
+          </TabsTrigger>
+          <TabsTrigger value="land-size" className="py-1">
+            <LandPlot className="w-5 h-5 mr-2" /> Land Size
+          </TabsTrigger>
+          <TabsTrigger value="farming-experience" className="py-1">
+            <Tractor className="w-5 h-5 mr-2" /> Farming Experience
+          </TabsTrigger>
+          <TabsTrigger value="other-income" className="py-1">
+            <DollarSign className="w-5 h-5 mr-2" /> Other Income
+          </TabsTrigger>
+          <TabsTrigger value="livestock" className="py-1">
+            <Cat className="w-5 h-5 mr-2" /> Livestock size
+          </TabsTrigger>
+          <TabsTrigger value="education" className="py-1">
+            <GraduationCap className="w-5 h-5 mr-2" /> Education
+          </TabsTrigger>
+          <TabsTrigger value="status" className="py-1">
+            <SlidersHorizontal className="w-4 h-4 mr-2" /> Status
+          </TabsTrigger>
+          <TabsTrigger value="others" className="py-1">
+            <Plus className="w-5 h-5 mr-2" /> Others
+          </TabsTrigger>
+        </TabsList>
+        <Separator />
+        <TabsContent value="summary">
+          <Summary />
+        </TabsContent>
+        <TabsContent value="land-size">
+          <LandSize />
+        </TabsContent>
+        <TabsContent value="farming-experience">
+          <FarmingExperience />
+        </TabsContent>
+        <TabsContent value="other-income">
+          <OtherIncome />
+        </TabsContent>
+        <TabsContent value="livestock">
+          <LivestockSize />
+        </TabsContent>
+        <TabsContent value="education">
+          <Education />
+        </TabsContent>
+        <TabsContent value="status">
+          <Status />
+        </TabsContent>
+        <TabsContent value="others">
+          <Others />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default AgrocChortsSettingsPage;
